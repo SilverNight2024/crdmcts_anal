10652c64e7091665dc6b50124d486c2831e34c0c
Simon Joseph <sejoseph@ucsd.edu>
2025-11-16 12:21:30 -0800

    Closing in

--- edge_detect.py
+++ edge_detect.py
@@ -32,50 +32,50 @@ def img_select(sup_keys, sub_keys, delim_str, nm_dct, img_dct, sel_vec):
           nm_lst[i] = nm_dct[tot_key][arr[2]]
      return nm_lst, img_arr
           
-def edge_det_test(nms, imgs):
+def mask_test(nms, imgs):
      testing = True
      while testing:
         bad = True
         while bad:
             print("\nPlease don't misnput I will break 👉👈")
             try:
-                im_idx = int(input("\nPlease input integer index of image" +
+                im_idx = int(input("\nPlease input integer index of image " +
                                     f"(not greater than {len(nms) -1}): "))
-                bl_size = int(input("\nPlease input bilateral filter size" + 
-                                     " (integer <10): "))
-                bl_s_cl = float(input("\nPlease input bilateral filter sigma" + 
-                                     " color value (float): "))
-                bl_s_sp = int(input("\nPlease input bilateral filter sigma" + 
-                                     " space size (integer): "))
+                g_sig_1 = int(input("\nPlease input first gaussian bg" + 
+                                     " estimation sigma (integer): "))
+                g_sig_2 = int(input("\nPlease input second gaussian bg" + 
+                                     " estimation sigma (integer): "))
                 clp_lim = float(input("\nPlease input CLAHE clip limit (float): "))
-                oc_k_sz = int(input("\nPlease input open/close kernel" + 
-                                     " size (integer): "))
-                fig, ((ori_plt, bl_8_plt), (clh_plt,msk_plt))\
+                fig, ((ori_plt, clh_1_plt), (clh_2_plt,msk_plt))\
                       = plt.subplots(2, 2)
                 ori_plt.imshow(imgs[im_idx,:,:])
                 ori_plt.set_title(nms[im_idx])
                 ori_plt.axis("off")
                 blur = cv.GaussianBlur(imgs[im_idx,:,:],\
-                                       (21,21),5)
-                blur_32 = blur.astype(np.float32) / 65535 #optimizes bilateral filter
-                bl = cv.bilateralFilter(blur_32,d=bl_size,sigmaColor = bl_s_cl\
-                                        ,sigmaSpace = bl_s_sp)
-                bl_8= cv.normalize(bl, None, 0, 255,\
+                                       (0,0),g_sig_1)
+                img_n_bg = cv.subtract(imgs[im_idx,:,:],blur)
+                img_no_bg_8= cv.normalize(img_n_bg, None, 0, 255,\
                                        cv.NORM_MINMAX).astype('uint8')
                 #normalize linearly maps pixels from 0 --> 255 to preserve
                 #gradients. .astype('uint8') allows cv to 
                 #actually read the image by converting back to integers
-                bl_8_plt.imshow(bl_8)
-                bl_8_plt.set_title("8-bit BL Filter after G Blur")
-                bl_8_plt.axis("off")
                 clh = cv.createCLAHE(clipLimit = clp_lim, tileGridSize = (8,8))
-                clahe = clh.apply(bl_8)
-                clh_plt.imshow(clahe)
-                clh_plt.set_title("8-bit CLAHE")
-                clh_plt.axis("off")
+                clahe = clh.apply(img_no_bg_8)
+                clh_1_plt.imshow(clahe)
+                clh_1_plt.set_title("8-bit CLAHE")
+                clh_1_plt.axis("off")
+                blur_2 = cv.GaussianBlur(clahe,\
+                                       (0,0),g_sig_2)
+                img_n_bg_2 = cv.subtract(clahe,blur_2)
+                img_no_bg_8_2= cv.normalize(img_n_bg_2, None, 0, 255,\
+                                       cv.NORM_MINMAX).astype('uint8')
+                clahe_2 = clh.apply(img_no_bg_8_2)
+                clh_2_plt.imshow(clahe_2)
+                clh_2_plt.set_title("8-bit CLAHE")
+                clh_2_plt.axis("off")
                 _, mask = cv.threshold(clahe, 0, 255,\
                                        cv.THRESH_BINARY + cv.THRESH_OTSU)
-                k = np.ones((oc_k_sz,oc_k_sz), np.uint8)
+                k = np.ones((5,5), np.uint8)
                 mask = cv.morphologyEx(mask, cv.MORPH_OPEN, k)
                 mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, k, iterations=2)
                 msk_plt.imshow(mask)
@@ -110,7 +110,7 @@ rep_img_sel_vec = np.array([[3,3,8],[3,4,0],[3,5,5],[3,1,2]])
 lif_fls, trns_nms = get_key_parts(img_Nms, delim_str)
 rep_nms, rep_imgs = img_select(lif_fls, trns_nms, delim_str,
                               img_Nms, imgs, rep_img_sel_vec)
-edge_det_test(rep_nms, rep_imgs)
+mask_test(rep_nms, rep_imgs)
 
     
 
