bad914a0a8a336ad140ef586e2a3673957baa8f4
Simon Joseph <sejoseph@ucsd.edu>
2025-11-18 00:43:09 -0800

    final mask code?

--- edge_detect.py
+++ edge_detect.py
@@ -1,8 +1,11 @@
-from open_lif import use_open_lif #to load in dicts
-from img_dict_manip import show_imgs, get_key_parts
+import os #To interpret file paths
+import pickle #To save the dicts
 import numpy as np #to manipulate images
 import matplotlib.pyplot as plt #plotting
 import cv2 as cv
+from prettytable import PrettyTable
+from open_lif import use_open_lif
+from img_dict_manip import show_imgs, get_key_parts
 
 def prompt_show_imgs(nm_dict, im_dict, delim_str):
     """Prompts user if they'd like to use show_imgs"""
@@ -31,7 +34,7 @@ def img_select(sup_keys, sub_keys, delim_str, nm_dct, img_dct, sel_vec):
           img_arr[i,:,:] = img_dct[tot_key][arr[2]]
           nm_lst[i] = nm_dct[tot_key][arr[2]]
      return nm_lst, img_arr
-          
+
 def mask_test(nms, imgs):
      testing = True
      while testing:
@@ -41,25 +44,21 @@ def mask_test(nms, imgs):
             try:
                 im_idx = int(input("\nPlease input integer index of image " +
                                     f"(not greater than {len(nms) -1}): "))
-                g_sig = int(input("\nPlease input gaussian bg" + 
-                                     " estimation sigma (integer): "))
-                clp_lim = float(input("\nPlease input CLAHE clip limit (float): "))
-                gamma = float(input("\nPlease input gamma correction (float): "))
-                loops = int(input("\nInput number of cleaning loops (int): "))
+                loops = int(input("\nPlease input number of loops (int): "))
                 fig, ((ori_plt, new_img_plt, msk_plt))\
                       = plt.subplots(1, 3)
                 ori_plt.imshow(imgs[im_idx,:,:])
                 ori_plt.set_title(nms[im_idx])
                 ori_plt.axis("off")
                 ori_g = (imgs[im_idx,:,:] / np.max(imgs[im_idx,:,:]))\
-                      ** gamma
+                      ** 0.5
                 ori_g_8 = cv.normalize(ori_g, None, 0, 255,\
                                        cv.NORM_MINMAX).astype('uint8')
-                clh = cv.createCLAHE(clipLimit = clp_lim, tileGridSize = (8,8))
+                clh = cv.createCLAHE(clipLimit = 1.0, tileGridSize = (8,8))
                 img = ori_g_8
                 for _ in range(loops):
                     blur = cv.GaussianBlur(img,\
-                                        (0,0),g_sig)
+                                        (0,0),80)
                     img = cv.subtract(img,blur)
                     img= cv.normalize(img, None, 0, 255,\
                                         cv.NORM_MINMAX).astype('uint8')
@@ -72,9 +71,6 @@ def mask_test(nms, imgs):
                 new_img_plt.axis("off")
                 _, mask = cv.threshold(img, 0, 255,\
                                        cv.THRESH_BINARY + cv.THRESH_OTSU)
-                k = np.ones((5,5), np.uint8)
-                # mask = cv.morphologyEx(mask, cv.MORPH_OPEN, k)
-                # mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, k, iterations=2)
                 msk_plt.imshow(mask)
                 msk_plt.set_title("Otsu Mask")
                 msk_plt.axis("off")
@@ -100,14 +96,76 @@ def mask_test(nms, imgs):
             else:
                 print("\nInvalid input, please try again.")
 
-img_Nms, imgs = use_open_lif()
-delim_str = "#"
-#prompt_show_imgs(img_Nms, imgs, delim_str)
-rep_img_sel_vec = np.array([[3,3,8],[3,4,0],[3,5,5],[3,1,2]])
-lif_fls, trns_nms = get_key_parts(img_Nms, delim_str)
-rep_nms, rep_imgs = img_select(lif_fls, trns_nms, delim_str,
-                              img_Nms, imgs, rep_img_sel_vec)
-mask_test(rep_nms, rep_imgs)
+def pkl_msks(dct):
+    #c_dir = os.getcwd()
+    pkl_msk_fnm = "cmc_msk_dct.pkl" #define pickle filename
+    with open(pkl_msk_fnm, "wb") as f: #pickle dicts
+        pickle.dump(dct, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-    
+def mask(img_in):
+    img = img_in.copy()
+    p99 = np.percentile(img, 99)
+    p95 = np.percentile(img, 95)
+    t = p99 - p95 #makes the gap the threshold for no signal
+    if t > 80: #empirical threshold with a little give 
+        img = (img / np.max(img))\
+            ** 0.5
+        img = cv.normalize(img, None, 0, 255,\
+                        cv.NORM_MINMAX).astype('uint8')
+        clh = cv.createCLAHE(clipLimit = 1.0, tileGridSize = (8,8))
+        for _ in range(5):
+            blur = cv.GaussianBlur(img,\
+                                (0,0),80)
+            img = cv.subtract(img,blur)
+            img= cv.normalize(img, None, 0, 255,\
+                                cv.NORM_MINMAX).astype('uint8')
+            img = clh.apply(img)
+            img = cv.equalizeHist(img)
+        img = cv.GaussianBlur(img, (0,0),4)
+        img = cv.fastNlMeansDenoising(img,None,5,7,21)
+        _, mask = cv.threshold(img, 0, 255,\
+                                cv.THRESH_BINARY + cv.THRESH_OTSU)
+    else:
+        mask = np.zeros(img.shape)
+    return mask
+
+def img_dct_2_msks(in_dct):
+    msk_dct = {}
+    in_dct_kys = list(in_dct.keys())
+    for _, k in enumerate(in_dct_kys):
+        tmp_arr = np.zeros(in_dct[k].shape)
+        for i in range(in_dct[k].shape[0]):
+            tmp_arr[i,:,:] =\
+            mask(in_dct[k][i,:,:])
+        msk_dct[k] = tmp_arr
+    print("All Masks Created")
+    return msk_dct
+
+rep_img_sel_vec = \
+np.array([
+    [3,3,8],
+    [3,4,0],
+    [3,5,5],
+    [3,1,2],
+    [0, 1, 17],
+    [0, 4, 16],
+    [0, 4, 9],
+    [3, 7, 10],
+    [3, 7, 11],
+    [8, 2, 18],
+    [8, 2, 19]
+])
+
+img_Nms, imgs = use_open_lif()
+# delim_str = "#"
+#prompt_show_imgs(img_Nms, imgs, delim_str)
+# lif_fls, trns_nms =\
+#       get_key_parts(img_Nms, delim_str)
+#sel_nm, sel_img = img_select(
+    # lif_fls, trns_nms, 
+    # delim_str, img_Nms, 
+    # imgs, rep_img_sel_vec)
+#mask_test(sel_nm, sel_img)
+msks = img_dct_2_msks(imgs)
+pkl_msks(msks)
 
