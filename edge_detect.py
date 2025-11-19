2318d803a15203a02ce06d41195072913e91695c
Simon Joseph <sejoseph@ucsd.edu>
2025-11-18 22:36:12 -0800

    God Willing this is the final commit for the masks

--- edge_detect.py
+++ edge_detect.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt #plotting
 import cv2 as cv
 from prettytable import PrettyTable
 from open_lif import use_open_lif
-from img_dict_manip import show_imgs, get_key_parts
+from img_dict_manip import show_imgs, get_key_parts, req_two_idx
 
 def prompt_show_imgs(nm_dict, im_dict, delim_str):
     """Prompts user if they'd like to use show_imgs"""
@@ -96,9 +96,14 @@ def mask_test(nms, imgs):
             else:
                 print("\nInvalid input, please try again.")
 
-def pkl_dct(dct,fnm):
-    with open(fnm, "wb") as f: #pickle dicts
-        pickle.dump(dct, f, protocol=pickle.HIGHEST_PROTOCOL)
+def pkl_upkl_dct(dct,fnm,bool):
+    """if Bool is true, write, if false, read"""
+    if bool:
+        with open(fnm, "wb") as f: #pickle dicts
+            pickle.dump(dct, f, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        with open(fnm, "rb") as f: #pickle dicts
+            return pickle.load(f)
 
 def mask(img_in):
     img = img_in.copy()
@@ -158,30 +163,178 @@ def img_dct_2_msks(in_dct,nm_dct,sup_strs,sub_strs,delim):
               "img_dct_2_msk directory vars and actual directory")
 
     if mk_msks:
-        print("\n Starting Mask Creation")
+        print("\nStarting Mask Creation")
         for sup_k in sup_strs: #gen sup_k dicts
             msk_dct = {}
             for sub_k in sub_strs:
                 k = sup_k + delim + sub_k
                 tmp_arr = np.zeros(in_dct[k].shape, dtype=np.uint8)
-                print("\n")
                 for i in range(in_dct[k].shape[0]): #gen dict
                     tmp_arr[i,:,:] =\
                     mask(in_dct[k][i,:,:])
-                    print(f"{nm_dct[k][i]} Mask Created")
                 msk_dct[k] = tmp_arr
             dct_nm = sup_k.replace(".lif",".pkl")
             t_dir = os.path.join(c_fld_dir,dct_nm)
-            pkl_dct(msk_dct,t_dir)
-            print(f"{dct_nm} Mask Dict Pickled")
+            pkl_upkl_dct(msk_dct,t_dir,True)
+            print(f"\n{dct_nm} Mask Dict Pickled")
 
+def comp_msk(nm_dict, img_dict, delim_str):
+    """Pared down the code in show_imgs to allow for just 
+    index based image opening in subplot with img and mask"""
+    sup_kys, sub_kys = get_key_parts(nm_dict, delim_str)
+    table_1 = PrettyTable()
+    table_1.field_names = ["Prompt Input", "First Prompt","Second Prompt"]
+    for i in range(max(len(sup_kys), len(sub_kys))): #defines table
+        if ((i < len(sup_kys)) & (i < len(sub_kys))):
+            tbl_row = [i, sup_kys[i], sub_kys[i]]
+        elif ((i < len(sup_kys)) & (i >= len(sub_kys))):
+            tbl_row = [i,sup_kys[i],"N/A"]
+        elif ((i >= len(sup_kys)) & (i < len(sub_kys))):
+            tbl_row = [i,"N/A",sub_kys[i]] 
+        table_1.add_row(tbl_row)
+    more_imgs = True #set while loop
+    sm_msk_b = False
+    while more_imgs: #allow for the opening of multiple sets of images
+        print("\nHere is a table with indexes associated with image files and transcript\n" +
+            "categories of the images openable through this function.")
+        print(table_1)
+        print("\nPlease put input corresponding to\n" +
+        "the prompt number and desired img key")
+        sup_kys_idx, sub_kys_idx = req_two_idx(len(sup_kys),len(sub_kys))
+        req_key = sup_kys[sup_kys_idx] + delim_str + sub_kys[sub_kys_idx]
+        img_mns = img_dict[req_key].mean(axis=(1,2)) #gets mean for brightness
+        if not(sm_msk_b): #get dict
+            c_dir = os.getcwd()
+            fld_dir = "msk_dcts"
+            c_fld_dir = os.path.join(c_dir,fld_dir)
+            msk_dct_nm = sup_kys[sup_kys_idx].replace('.lif','.pkl') #get mask name
+            msk_dct = pkl_upkl_dct(None,os.path.join(c_fld_dir,msk_dct_nm),False)  
+        msk_mns = msk_dct[req_key].mean(axis=(1,2))
+        print("\nHere is a table with the index associated with,\n" +
+                    "name of, and brightness of all slected images.")
+        gap_arr = np.zeros(img_dict[req_key].shape[0])
+        for i in range(img_dict[req_key].shape[0]): #generate gap array
+            img = img_dict[req_key][i,:,:].copy()
+            p99 = np.percentile(img, 99)
+            p95 = np.percentile(img, 95)
+            gap_arr[i] = p99 - p95
+        table = PrettyTable()
+        table.add_column("Index",np.arange(0,len(img_mns)))
+        table.add_column("Image Name",nm_dict[req_key])
+        table.add_column("Mean Brightness",img_mns)
+        table.add_column("99-95 gap",gap_arr)
+        table.add_column("Mask Mean",msk_mns)
+        print(table)
+        bad = True
+        while bad: #prompt to open more images
+            img_opn = input("\nWould You like to Open Images? (Y/N): ")
+            if img_opn.capitalize() == "Y": #Open more images if yes
+                show_imgs = True
+                bad = False
+            elif img_opn.capitalize() == "N": #open no images if no
+                show_imgs = False
+                bad = False
+            else: #retry
+                print("\nInvalid input, please try again.")
+        if show_imgs:
+            max_idx = len(nm_dict[req_key])
+            bad = True
+            while bad: #makes sure indexes are correct
+                print("\nYou will be asked to input first and second indexes\n" +
+                    "These will be the starting and ending integer indexes\n" +
+                    "of images in the selsection you would like to open.\n" +
+                    "The first index can thus equal but NOT exceed the second.\n" +
+                    f"They also cannot be less than 0 or greater than {max_idx-1}.")
+                idx_s, idx_e = req_two_idx(max_idx,max_idx)   
+                if idx_s <= idx_e:
+                    proc_q = input(f"\nThis will generate {idx_e-idx_s+1} imgs. Proceed? (Y/N): ")
+                    if proc_q .capitalize() == "Y": #Open images is yes
+                        print("\nNOTE: You will have to close all image windown before continuing")
+                        bad = False
+                    else:
+                        print("\nWill not proceed, returning to indexes prompt.")
+            img_sel = img_dict[req_key][np.arange(idx_s,idx_e+1)]
+            msk_sel = msk_dct[req_key][np.arange(idx_s,idx_e+1)]
+            for i in range(img_sel.shape[0]):
+                fig, ((ori_plt, msk_plt)) = plt.subplots(1, 2)
+                fig.canvas.manager.set_window_title(nm_dict[req_key][i+idx_s])  # real window title
+                ori_plt.imshow(img_sel[i]) 
+                ori_plt.axis("off")
+                ori_plt.set_title(nm_dict[req_key][i+idx_s])
+                msk_plt.imshow(msk_sel[i]) 
+                msk_plt.axis("off")
+                msk_plt.set_title("Mask")
+            plt.show()
+        bad = True
+        while bad: #ask if the user wants to open more images
+            go_on = input("\nContinue to look at masks? (Y/N): ")
+            if go_on.capitalize() == "Y": #Open more images if yes
+                sm_msk = input("\nWill you still be looking at\n" + 
+                           f"{sup_kys[sup_kys_idx]}? (Y/N):")
+                if sm_msk.capitalize() == "Y": #Open more images if yes
+                    sm_msk_b = True
+                    more_imgs = True
+                    bad = False
+                elif sm_msk_b.capitalize() == "N":
+                    del(msk_dct)
+                    sm_msk_b = False
+                    more_imgs = False
+                    bad = False
+                else:
+                    print("\nInvalid input, please try again.")
+                bad = False
+            elif go_on.capitalize() == "N":
+                more_imgs = False
+                bad = False
+            else:
+                print("\nInvalid input, please try again.")
+
+def msk_dta_sum_tbls(nm_dict, img_dict, delim_str):
+    """No functionality but printing the comparison info"""
+    sup_kys, sub_kys = get_key_parts(nm_dict, delim_str)
+    c_dir = os.getcwd()
+    fld_dir = "msk_dcts"
+    c_fld_dir = os.path.join(c_dir,fld_dir)
+    for i in range(len(sup_kys)):
+        msk_dct_nm = sup_kys[i].replace('.lif','.pkl') #get mask name
+        msk_dct = pkl_upkl_dct(None,os.path.join(c_fld_dir,msk_dct_nm),False)
+        dngn = True
+        while dngn: #stop to inspect
+            go_on = input(f"\nGenerate tables for {sup_kys[i]}? (Y/N): ")
+            if go_on.capitalize() == "Y":
+                dngn = False
+            elif go_on.capitalize() == "N":
+                return()
+            else:
+                print("\nIncorrect input. Please try again.")  
+        for j in range(len(sub_kys)):
+            k = sup_kys[i] + delim_str + sub_kys[j]
+            msk_mns = msk_dct[k].mean(axis=(1,2))
+            img_mns = img_dict[k].mean(axis=(1,2))
+            gap_arr = np.zeros(img_dict[k].shape[0])
+            print("\n")
+            for l in range(img_dict[k].shape[0]): #generate gap array
+                img = img_dict[k][l,:,:].copy()
+                p99 = np.percentile(img, 99)
+                p95 = np.percentile(img, 95)
+                gap_arr[l] = p99 - p95
+            table = PrettyTable()
+            table.add_column("Index",np.arange(0,len(img_mns)))
+            table.add_column("Image Name",nm_dict[k])
+            table.add_column("Mean Brightness",img_mns)
+            table.add_column("99-95 gap",gap_arr)
+            table.add_column("Mask Mean",msk_mns)
+            print(table)
+            
 img_Nms, imgs = use_open_lif()
 delim_str = "#"
-lif_fls, trns_nms =\
-      get_key_parts(img_Nms, delim_str)
-img_dct_2_msks(imgs, img_Nms, 
-               lif_fls, trns_nms, 
-               delim_str)
+# lif_fls, trns_nms =\
+#       get_key_parts(img_Nms, delim_str)
+#img_dct_2_msks(imgs, img_Nms, 
+            #    lif_fls, trns_nms, 
+            #    delim_str)
+comp_msk(img_Nms, imgs, delim_str)
+
 
 # rep_img_sel_vec = \
 # np.array([
